5c3f189e3773e9a1f9abab10d289c2108b2699ff
William Ray Johnson <willrayjohnson@gmail.com>
2018-03-06 01:15:14 -0600

    Implemented decodePacketData

--- src/SHAC/protocol/SHACData.java
+++ src/SHAC/protocol/SHACData.java
@@ -3,15 +3,21 @@ package SHAC.protocol;
 import java.util.ArrayList;
 
 public class SHACData {
-    public static final String VERSION = "SHC1.0";
+    public static final String CURRENT_VERSION = "SHC1.0";
+    public String version;
     public int nodeCount;
     public NodeType nodeTypeFlag;
     public ArrayList<SHACNode> nodes;
     
     public SHACData(int nodeCount, NodeType nodeTypeFlag) {
+        this.version = CURRENT_VERSION;
         this.nodeCount = nodeCount;
         this.nodeTypeFlag = nodeTypeFlag;
         if(nodeCount > 0)
             nodes = new ArrayList<SHACNode>();
+    }
+    
+    public void setVersion(String version) {
+        this.version = version;
     }
 }
