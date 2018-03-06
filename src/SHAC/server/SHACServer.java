e261137c1beca3b11519a78aa8c9929fc6472a12
Jonathan Rogers <jpr8237@truman.edu>
2018-03-06 16:38:35 -0600

    I have committed more now that I have eliminated the original errors from my first commit

--- src/SHAC/server/SHACServer.java
+++ src/SHAC/server/SHACServer.java
@@ -3,36 +3,50 @@ import java.io.IOException;
 import java.util.Date;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import SHAC.protocol.*;
 
 public class SHACServer extends Thread{
 	
-
+	private String threadName;
+    private DatagramSocket socket;
+    public ArrayList<SHACNode> nodes;
+	
 	public void run() {
-
+		
+		System.out.println("Running " + threadName);
+		
+		if (threadName.equals("Server")){
+			createAndListenSocket();
+		}
+		
+	}
+	
+	SHACServer(){ 
+		initializeServer();
+	}
+	
+	public void initializeServer(){
+		nodes = new ArrayList<SHACNode>();
 	}
 	
 	public void createAndListenSocket() 
-    {
-		 DatagramSocket socket = new DatagramSocket();
-		
+    {	 	
 		while (true) {
 			try {
+				DatagramSocket socket = new DatagramSocket(9649);
 				byte[] incomingData = new byte[1024];
 				DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-				
 				
 				socket.receive(incomingPacket);
 				SHACData data = SHACProtocol.decodePacketData(incomingPacket.getData());
 
-				SHACNode sender = new SHACNode(incomingPacket.getAddress(), new Date());
+				SHACNode sender = new SHACNode(incomingPacket.getAddress(), new Date());				
 				data.nodes.add(sender);
 
 				boolean listChanged = false;
-				if (data.nodeTypeFlag == NodeType.PEER) {
+				if (data.nodeTypeFlag == NodeType.CLIENT) {
 					for (SHACNode newNode : (SHACNode[]) data.nodes.toArray()) {
 						if (!nodes.contains(newNode)) {
 							this.nodes.add(newNode);
@@ -49,8 +63,6 @@ public class SHACServer extends Thread{
 				if (!listChanged) {
 					sendUpdates();
 				}
-				System.out.println("Received availability update from server.");
-				socket.close();
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			} catch (SocketException e) {
@@ -60,13 +72,31 @@ public class SHACServer extends Thread{
 			}
 		}
 	}
+	
+    private void sendUpdates() {
+        for (SHACNode node : (SHACNode[]) nodes.toArray()) {
+            // Send all updates to all clients your full list of peers
+            try {
+                SHACData update = new SHACData(nodes.size(), NodeType.CLIENT);
+                update.nodes = nodes;
+                byte[] data = SHACProtocol.encodePacketData(update);
+                DatagramPacket sendPacket = new DatagramPacket(data, data.length, node.ip, 9876);
+                socket.send(sendPacket);
+                System.out.println("Message sent to client");
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }    
+    
+    
     
     public static void main(String[] args) {  
             SHACServer server = new SHACServer();
             server.createAndListenSocket();
-        
-
-    }
-    
-
+    }    
 }
